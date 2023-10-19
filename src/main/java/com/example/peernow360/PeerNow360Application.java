34f2b4e5f8329944218d77c802cc9f307c55f68d
gitgusgml042933 <gitgusgml042933@gmail.com>
2023-10-19 16:00:25 +0900

    init commit

---
+++ src/main/java/com/example/peernow360/PeerNow360Application.java
@@ -0,0 +1,13 @@
+package com.example.peernow360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerNow360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerNow360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/peernow360/PeerNow360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peernow360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerNow360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
