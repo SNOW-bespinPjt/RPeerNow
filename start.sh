a6e80b73df43a808d81a1ce69140472a91a53523
양성수 <112933943+ystgd07@users.noreply.github.com>
2023-11-01 18:05:20 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ec2-user/spring"
-JAR="$ROOT_PATH/application.jar"
+JAR="$ROOT_PATH/application.war"
 
 APP_LOG="$ROOT_PATH/application.log"
 ERROR_LOG="$ROOT_PATH/error.log"
@@ -10,7 +10,7 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/PeerNow360-0.0.1-SNAPSHOT.jar $JAR
+cp $ROOT_PATH/build/libs/PeerNow360-0.0.1-SNAPSHOT.war $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
