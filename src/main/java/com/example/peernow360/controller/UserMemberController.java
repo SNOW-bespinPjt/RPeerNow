96c0e1daddd0a4e697f6fe9bbd3017ffd059e439
양성수 <112933943+ystgd07@users.noreply.github.com>
2023-11-01 15:43:28 +0900

    Update UserMemberController.java

--- src/main/java/com/example/peernow360/controller/UserMemberController.java
+++ src/main/java/com/example/peernow360/controller/UserMemberController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 @RestController
 @Log4j2
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserMemberController {
 
     private final UserMemberService userMemberService;
