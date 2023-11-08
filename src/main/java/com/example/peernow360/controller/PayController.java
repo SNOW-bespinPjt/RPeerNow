0c6db9195ffa020c3205fb518b52976ca2d01ac8
gitgusgml042933 <gitgusgml042933@gmail.com>
2023-11-08 12:53:35 +0900

    Feat pay branch

---
+++ src/main/java/com/example/peernow360/controller/PayController.java
@@ -0,0 +1,17 @@
+package com.example.peernow360.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/api/pay")
+@Tag(name = "pay", description = "결제")
+public class PayController {
+
+
+}
