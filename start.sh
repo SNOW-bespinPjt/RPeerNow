5b3318e79888b87d866defffcffc6f6d9f0862c8
양성수 <112933943+ystgd07@users.noreply.github.com>
2023-11-17 18:07:39 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/PeerNow360-0.0.1-SNAPSHOT.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > /dev/null 2> &1 &
+nohup java -jar $JAR 1>/dev/null 2>&1 &
 
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
